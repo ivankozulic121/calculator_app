6df7805c8eda92eca176733008fce5325a61ed5b
ivank <ivankozulic121@gmail.com>
2025-04-05 06:58:06 +0200

    fixed output.jsx file

---
+++ src/components/Output.jsx
@@ -0,0 +1,8 @@
+
+
+const Output = ({output}) =>{
+
+    return (
+      <div className="w-120 h-32 rounded-lg font-spartan text-right bg-output text-6xl text-outputText flex flex-row justify-end items-center pr-8 mt-5">{output}</div>
+    )
+  }
